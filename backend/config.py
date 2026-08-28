a6dfce13a56cbbefbd40a437550d26a9fa86eb5d
EzraelJeremiah-py <ezraeljeremiah@outlook.com>
2026-08-28 15:54:15 +0300

    Modify environment loading based on ENV variable

--- backend/config.py
+++ backend/config.py
@@ -1,10 +1,13 @@
 import os
 from dotenv import load_dotenv
 
-# Load environment file
-load_dotenv(".env.prod")
+ENV = os.getenv("ENV", "dev")
+
+if ENV == "prod":
+    load_dotenv(".env.prod")
+else:
+    load_dotenv(".env.dev")
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 SECRET_KEY = os.getenv("SECRET_KEY")
-DEBUG = os.getenv("DEBUG", "False") == "True"
-
+DEBUG = os.getenv("DEBUG", "True") == "True"
